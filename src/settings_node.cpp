b65866b58beddc3f1a9f5d13d60302121fad0da8
Robert Hedman <robert.hedman@mac.com>
2018-10-11 07:59:55 +0000

    fixed working thow ther is a wind up bug

--- src/settings_node.cpp
+++ src/settings_node.cpp
@@ -16,7 +16,7 @@ struct toggleButton {
 };
 
 struct toggleButton emergensy_stop = {.state = true, .joy_last = false}; // A
-struct toggleButton saft_blandare = {.state = false, .joy_last = false}; // Y
+struct toggleButton saft_blandare = {.state = true, .joy_last = false}; // Y
 
 void pubData();
 void dataCallback(const std_msgs::Int8MultiArray::ConstPtr& array);
@@ -42,31 +42,36 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 void toggel(int val, struct toggleButton *b)
 {
 	if (val == 1 && !b->joy_last)
-			b->state = !b->state;
+		b->state = !b->state;
 	b->joy_last = (val == 1);
 }
 
 
 void pubData()
 {
-		p_data.data[0] = emergensy_stop.state;
-		p_data.data[1] = emergensy_stop.state;
-		p_data.data[2] = saft_blandare.state;
-		p_data.data[3] = 0;
+	p_data.data.push_back(emergensy_stop.state);
+	p_data.data.push_back(emergensy_stop.state);
+	p_data.data.push_back(saft_blandare.state);
+	p_data.data.push_back(0);
 
-		p_relay.publish(p_data);
+	p_relay.publish(p_data);
+	p_data.data.clear();
 }
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, NODE_NAME);
 	ros::NodeHandle n;
+	
+	ros::Rate loop_rate(100);	
 	p_relay = n.advertise<std_msgs::Int8MultiArray>(PUB_CHEANEL, 10);
 	ros::Subscriber s_data = n.subscribe<std_msgs::Int8MultiArray>(SUB_CHANEL, 10, dataCallback);
 	ros::Subscriber s_joy = n.subscribe<sensor_msgs::Joy>(JOY_SUB_NODE, 200, joyCallback);
-
-	ros::spinOnce();
-	//loop_rate.sleep();
-
+	
+	while(ros::ok())
+	{
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
 	return 0;
 }
